9669ea51569993f07c9ef9d405f682c0958cf1de
Sam Van Den Berge <sam.vandenberge@altran.com>
2016-01-30 16:02:09 +0100

    Fix create_vde.sh

--- create_vde.sh
+++ create_vde.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
-vde_switch -s /tmp/pic0.ctl -m 777 -M /tmp/pico.mgmt -d -hub -t vde0
-vde_switch -s /tmp/pic1.ctl -m 777 -M /tmp/pici.mgmt -d -hub
+vde_switch -s /var/run/vde.ctl -m 777 -M /var/run/pico.mgmt -d -hub -t vde0
 ifconfig vde0 10.40.0.1 netmask 255.255.0.0
 
-
-
 sudo ip addr add 2001:db8:0:1::1000/64 dev vde0
-sudo dhcpd -6 -cf ./dhcpd6.conf
+sudo cp ./pico-dhcpd6.conf /etc/dhcp/pico-dhcpd6.conf
+sudo dhcpd -6 -cf /etc/dhcp/pico-dhcpd6.conf
